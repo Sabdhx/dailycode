aff611a224a1d6a93906a6835d5e02ff753a0149
Sabdhx <syedaliabdullahshah5@gmail.com>
2025-03-29 22:51:31 +0500

    changes

--- preperation/index.js
+++ preperation/index.js
@@ -46,17 +46,38 @@
 // counter(); // 3
 
 // promises are used to run the async funtion more correctly then callback;
-const isOpen = "true";
+// const isOpen = "true";
 
-const learningPromises = new Promise((resolve,reject)=>{
-  setTimeout(() => {
-    if(isOpen === "true"){
-      resolve("this isOpen is set to true")
-    }else{
-      reject("this isOpen is set to false")
-    }
-  }, 2000);
-})
+// const learningPromises = new Promise((resolve,reject)=>{
+//   setTimeout(() => {
+//     if(isOpen === "true"){
+//       resolve("this isOpen is set to true")
+//     }else{
+//       reject("this isOpen is set to false")
+//     }
+//   }, 2000);
+// })
 
 
-learningPromises.then((data)=>console.log("true " +data)).catch((error)=>console.log("error "+error)).finally(()=>console.log("this one will run in every situation"))
+// learningPromises.then((data)=>console.log("true " +data)).catch((error)=>console.log("error "+error)).finally(()=>console.log("this one will run in every situation"))
+
+// const dataFetching = async()=>{
+//   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+//   .then(response => response.json()) // Convert response to JSON
+//   .then(data => console.log(data))   // Handle data
+//   .catch(error => console.log("Error:", error)); // Handle errors
+// }
+// dataFetching()
+
+
+function greet(name, callback) {
+  console.log("Hello, " + name);
+  callback();
+}
+
+function sayGoodbye() {
+  console.log("Goodbye!");
+}
+
+greet("Alice", sayGoodbye);
+
